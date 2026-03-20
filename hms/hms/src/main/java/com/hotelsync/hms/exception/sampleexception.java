a5b6c98e91c01cbf4f3653c3ab31bd6aaac1f609
sma7-code <mantalcodex07@gmail.com>
2026-03-20 13:11:49 +0530

    feat: feature completed

--- hms/hms/src/main/java/com/hotelsync/hms/exception/sampleexception.java
+++ hms/hms/src/main/java/com/hotelsync/hms/exception/sampleexception.java
@@ -2,6 +2,6 @@ package com.hotelsync.hms.exception;
 
 public class sampleexception {
 
-    // this is what you have to find it on a git ok sumit;
+    // this is what you have to find it on a git ok sut;
 
 }
