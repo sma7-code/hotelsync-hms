682fde12cc7adcf346b8050b7cec18acd1b89201
sma7-code <mantalcodex07@gmail.com>
2026-04-03 18:08:13 +0530

    ticket 204 try 1

--- hms/hms/src/main/java/com/hotelsync/hms/controller/AuthController.java
+++ hms/hms/src/main/java/com/hotelsync/hms/controller/AuthController.java
@@ -40,7 +40,7 @@ public class AuthController {
     }
 
     @PostMapping("/change-password")
-    public ResponseEntity<?> changePassword(@Valid @RequestBody ChangePasswordRequest request){
+    public ResponseEntity<?> changePassword(@Valid @RequestBody ChangePasswordRequest request) throws IllegalAccessException {
         String response = authService.changePassword(request);
         return ResponseEntity.ok(response);
     }
