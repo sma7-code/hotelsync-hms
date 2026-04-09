ee6af67d66c768b0a59e9e8f9a12e8cce77b9f0e
sma7-code <mantalcodex07@gmail.com>
2026-04-09 15:35:39 +0530

    Ticket no 303 try 1

---
+++ hms/hms/src/main/java/com/hotelsync/hms/dto/UpdateUserRequest.java
@@ -0,0 +1,21 @@
+package com.hotelsync.hms.dto;
+
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UpdateUserRequest {
+
+    private String name;
+    private String email;
+    private String phone;
+
+
+}
--- hms/hms/src/main/java/com/hotelsync/hms/repository/UserRepository.java
+++ hms/hms/src/main/java/com/hotelsync/hms/repository/UserRepository.java
@@ -12,6 +12,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUserId(String userId);
 
+    Optional<User> findById(Long Id);
+
     Optional<User> findByEmail(String email);
 
     List<User> findByRole (Role role);
