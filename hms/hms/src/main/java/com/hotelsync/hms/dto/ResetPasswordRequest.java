df365839f3f39ec74af758521da42283a9e96220
sma7-code <mantalcodex07@gmail.com>
2026-04-10 17:37:45 +0530

    305 try 2

---
+++ hms/hms/src/main/java/com/hotelsync/hms/dto/ResetPasswordRequest.java
@@ -0,0 +1,17 @@
+package com.hotelsync.hms.dto;
+
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResetPasswordRequest {
+
+    @NotBlank(message = "Password Must Be Required to change")
+    private String password;
+
+}
